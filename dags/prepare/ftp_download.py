32abee5c537e755151dc035756904b28529993c4
aspesivtsev <anspesiv@yahoo.com>
2023-10-07 01:12:12 +0300

    added ftp test download

--- dags/prepare/ftp_download.py
+++ dags/prepare/ftp_download.py
@@ -2,10 +2,7 @@ from ftplib import FTP
 from datetime import datetime
 
 
-#credentials for accessing FTP
-ftp_host = 'ftp.horseforce.ru'
-username = 'ftp_user2'
-password = 'K8OCG7fSGlO3'
+
 
 ftp = FTP(ftp_host)
 ftp.login(user=username, passwd=password) 
