33d2fb0e0393d590750fbf3b17b27c19ac3f3908
aspesivtsev <anspesiv@yahoo.com>
2023-10-06 12:17:54 +0300

    changed volume for db 15 to opt and gitignore to skip db folder

--- dags/empty_dag.py
+++ dags/empty_dag.py
@@ -5,8 +5,9 @@ from airflow.operators.empty import EmptyOperator
 
 my_dag = DAG(
     dag_id="my_dag_name",
-    start_date=datetime.datetime(2021, 1, 1),
+    start_date=datetime.datetime(2023, 11, 1),
     schedule="@daily",
+    catchup=False
 )
 
 EmptyOperator(task_id="task", dag=my_dag)
