c65b22d1bf112f85a406c8d771d057bfc895ee9c
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-10-10 19:09:50 +0300

    Update to_orders_realization.py

--- dags/dds/to_orders_realization.py
+++ dags/dds/to_orders_realization.py
@@ -1,1 +1,251 @@
+import logging
+from logging import Logger
+from typing import List, Optional
+from lib import PgConnect
+from psycopg import Connection
+from psycopg.rows import class_row
+from pydantic import BaseModel
+from datetime import datetime
 
+
+log = logging.getLogger(__name__)
+
+CREATE TABLE product_category (
+	item_number varchar not NULL,          ####
+	product_name varchar not NULL,
+	brand varchar not NULL,
+	product_category varchar NULL
+
+CREATE TABLE new_sales(
+	manager_id int4 null,
+	manager varchar null,
+	client_id varchar not null,
+	client varchar not null,
+	sales_channel varchar null,
+	region varchar null,
+	order_date varchar null,        ####
+	order_number varchar null,         ####
+	realization_date varchar null,        ####
+	realization_number varchar null,        #####
+	product_id varchar not null,
+	item_number varchar not null,          ####
+	product_name varchar not null,
+	brand varchar null,
+	count int null,                              ####
+	price numeric(14, 2) null,                     ####
+	total_sum numeric(14, 2) null,                    #####
+	comment varchar null                                  ####
+
+
+  
+CREATE TABLE orders_realizations (
+	id int4 PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+	order_date date NOT null,
+	order_number varchar not null,
+  realization_date date NOT null,
+	realization_number varchar not null,
+	item_number varchar not null,
+	count int not null,
+	price numeric(14, 2) not null,
+	total_sum numeric(14, 2) not null,
+	comment varchar null
+
+
+
+  
+
+
+class ProductRawObj(BaseModel):
+    item_number: varchar 
+	  product_name: varchar
+	  brand: varchar
+	  product_category: varchar
+
+
+class ProductDdsObj(BaseModel):
+    item_number: varchar
+
+
+class SaleRawObj(BaseModel):
+    manager_id: int
+	  manager: varchar 
+	  client_id varchar not null,
+	  client varchar not null,
+	  sales_channel varchar null,
+	  region varchar null,
+	  order_date varchar null,        ####
+	  order_number varchar null,         ####
+	  realization_date varchar null,        ####
+	  realization_number varchar null,        #####
+	  product_id varchar not null,
+	  item_number varchar not null,          ####
+	  product_name varchar not null,
+	  brand varchar null,
+	  count int null,                              ####
+	  price numeric(14, 2) null,                     ####
+	  total_sum numeric(14, 2) null,                    #####
+	  comment varchar null                                  ####
+
+
+
+class SaleDdsObj(BaseModel):
+    manager_id: int
+	  manager: varchar 
+	  client_id varchar not null,
+	  client varchar not null,
+	  sales_channel varchar null,
+	  region varchar null,
+	  order_date varchar null,        ####
+	  order_number varchar null,         ####
+	  realization_date varchar null,        ####
+	  realization_number varchar null,        #####
+	  product_id varchar not null,
+	  item_number varchar not null,          ####
+	  product_name varchar not null,
+	  brand varchar null,
+	  count int null,                              ####
+	  price numeric(14, 2) null,                     ####
+	  total_sum numeric(14, 2) null,                    #####
+	  comment varchar null                                  ####
+
+
+CREATE TABLE sales (
+	id int4 PRIMARY KEY NOT NULL GENERATED ALWAYS AS IDENTITY,
+	id_manager int4 not null, –это поле берем из dds.managers
+	client_id varchar not null,
+	order_number varchar not null,
+	realization_number varchar not null,
+	item_number varchar not null,
+	count int not null,
+	total_sum numeric(14, 2) not null
+
+
+class ManagerRawObj(BaseModel):
+    manager_id: int
+    manager: varchar
+
+
+class ManagerDdsObj(BaseModel):
+    manager_id: int
+    manager: varchar
+
+
+class ManagerRawRepository:
+    def load_raw_manager(self, conn: Connection) -> List[ManagerRawObj]:
+    with conn.cursor(row_factory=class_row(ManagerRawObj)) as cur:
+            cur.execute(
+                """
+                    SELECT
+                        manager
+                    FROM stg.new_sales
+                """,
+                )
+            objs = cur.fetchall()
+        return objs
+
+
+
+class ManagerDdsRepository:
+    def insert_manager(self, conn: Connection, manager: ManagerDdsObj) -> None:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                    INSERT INTO dds.managers(manager)
+                    VALUES (%(manager)s)
+                    
+                """,
+                {
+                    "manager": manager.manager,
+               },
+            )
+
+
+class ManagerLoader:
+    def __init__(self, pg_conn: PgConnect, log: Logger) -> None:
+        self.conn = pg_conn
+        self.dds = ManagerDestRepository()
+        self.raw = ManagerRawRepository()        
+        self.log = log
+
+
+
+
+class ClientRawRepository:
+    def load_raw_client(self, conn: Connection) -> List[ClientRawObj]:
+    with conn.cursor(row_factory=class_row(ClientRawObj)) as cur:
+            cur.execute(
+                """
+                    SELECT
+                        client_id,
+                        client,
+                        sales_channel,
+                        region
+                    FROM stg.new_sales
+                """,
+                )
+            objs = cur.fetchall()
+        return objs
+
+
+class ClientDdsRepository:
+    def insert_client(self, conn: Connection, client: ClientDdsObj) -> None:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                    INSERT INTO dds.clients(client_id, client, sales_channel, region)
+                    VALUES (%(client_id)s, %(client)s, %(sales_channel)s, %(region)s)
+                    
+                """,
+                {
+                    "client_id": clients.client_id,
+                    "client": clients.client,
+                    "sales_channel": clients.sales_channel,
+                    "region": clients.region 
+                },
+            )
+
+
+class ManagerRawRepository:
+    def load_raw_manager(self, conn: Connection) -> List[ManagerRawObj]:
+    with conn.cursor(row_factory=class_row(ManagerRawObj)) as cur:
+            cur.execute(
+                """
+                    SELECT
+                        manager
+                    FROM stg.new_sales
+                """,
+                )
+            objs = cur.fetchall()
+        return objs
+
+
+
+class ManagerDdsRepository:
+    def insert_manager(self, conn: Connection, manager: ManagerDdsObj) -> None:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                    INSERT INTO dds.clients(manager)
+                    VALUES (%(manager)s)
+                    
+                """,
+                {
+                    "manager": clients.manager,
+               },
+            )
+
+
+class ClientLoader:
+    def __init__(self, pg_conn: PgConnect, log: Logger) -> None:
+        self.conn = pg_conn
+        self.dds = CourierDestRepository()
+        self.raw = CourierRawRepository()        
+        self.log = log
+
+
+class ManagerLoader:
+    def __init__(self, pg_conn: PgConnect, log: Logger) -> None:
+        self.conn = pg_conn
+        self.dds = ManagerDestRepository()
+        self.raw = ManagerRawRepository()        
+        self.log = log
