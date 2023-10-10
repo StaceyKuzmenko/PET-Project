ec2d69c9b6c673f0c74b84c4b65e27eb3bfe2e8e
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-10-11 02:08:06 +0300

    Update manager_dag1.py

--- dags/dds/manager_dag1.py
+++ dags/dds/manager_dag1.py
@@ -17,6 +17,15 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.utils.task_group import TaskGroup
 
+import pendulum
+from datetime import datetime
+from airflow.decorators import dag, task
+from airflow.operators.dummy import DummyOperator
+from typing import Generator
+import psycopg2
+from airflow.hooks.base import BaseHook
+from configparser import ConfigParser
+
 ### POSTGRESQL settings ###
 # set postgresql connection from basehook
 # all of these connections should be in Airflow as connectors
@@ -50,7 +59,7 @@ conn_1 = psycopg2.connect(
 def load_managers_to_dds():
     # fetching time UTC and table
     fetching_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    current_table = 'managerss'
+    current_table = 'managers'
     
     # load to local to DB (managers)
     cur_1 = conn_1.cursor()                 
@@ -66,30 +75,26 @@ def load_managers_to_dds():
 
 
 default_args = {
-    'owner': 'Airflow',
-    'schedule_interval':'@once',           # sheduled or not
-    'retries': 1,                          # the number of retries that should be performed before failing the task
-    'retry_delay': timedelta(minutes=5),   # delay between retries
-    'depends_on_past': False,
-    'catchup':False
+    "owner": "airflow",
+    "start_date": datetime(2023, 1, 1),
 }
 
-with DAG(
-        'managers_dag',                    # name
-        default_args=default_args,         # connect args
-        schedule_interval=None,  # interval
-        start_date=datetime(2023, 10, 10),   # start calc
-        catchup=False,                     # used in  the first launch, from date in the past until now. Usually = off
-        tags=['PET-Project', 'dds'],
-) as dag:
+dag = DAG(
+    dag_id="manager_dag1",
+    schedule_interval=None,
+    start_date=pendulum.datetime(2023, 9, 1),
+    catchup=False,
+    tags=["PET-Project", "dds"],
+    is_paused_upon_creation=False
+)
 
     # create DAG logic (sequence/order)
-    t1 = DummyOperator(task_id="start")
-    t21 = PythonOperator(task_id="managers", python_callable=load_managers_to_dds, dag=dag)
+t1 = DummyOperator(task_id="start")
+t21 = PythonOperator(task_id="managers", python_callable=load_managers_to_dds, dag=dag)
 #   t22 = PythonOperator(task_id="couriers", python_callable=load_paste_data_couriers, dag=dag)
 #   t23 = PythonOperator(task_id="timestamps", python_callable=load_paste_data_timestamps, dag=dag)
 #   t24 = PythonOperator(task_id="orders", python_callable=load_paste_data_orders, dag=dag)
 #   t25 = PythonOperator(task_id="deliveries", python_callable=load_paste_data_deliveries, dag=dag)
-    t4 = DummyOperator(task_id="end")
+t4 = DummyOperator(task_id="end")
     
-    t1 >> t21 >> t4
+t1 >> t21 >> t4
