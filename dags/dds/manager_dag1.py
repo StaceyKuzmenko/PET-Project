3f03fedac4539b93ed1c0d4a5c41e058850351be
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-10-11 03:46:22 +0300

    Update manager_dag1.py

--- dags/dds/manager_dag1.py
+++ dags/dds/manager_dag1.py
@@ -31,7 +31,7 @@ from contextlib import contextmanager
 # set postgresql connection from basehook
 # all of these connections should be in Airflow as connectors
 
-#pg_conn_1 = PostgresHook.get_connection('postgres_db_conn')
+# pg_conn_1 = PostgresHook.get_connection('postgres_db_conn')
 
 # init connection
 # Connect to your local postgres DB (Docker)
@@ -42,29 +42,27 @@ DB_PASS = "project_password"
 DB_HOST = "95.143.191.48"
 DB_PORT = "5433"
 
-conn = psycopg2.connect(database=DB_NAME,
-                            user=DB_USER,
-                            password=DB_PASS,
-                            host=DB_HOST,
-                            port=DB_PORT)
+conn_1 = psycopg2.connect(
+    database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
+)
 
 # load data from STG
 # paste data to DDS local connection
 # MANAGERS TABLE
 def load_managers_to_dds():
     # fetching time UTC and table
-    fetching_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    current_table = 'managers'
-    
+    fetching_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_table = "managers"
+
     # load to local to DB (managers)
-    cur_1 = conn_1.cursor()                 
+    cur_1 = conn_1.cursor()
     postgres_insert_query = """ 
     SELECT manager
     FROM stg.old_sales(manager)
     INSERT INTO dds.managers(manager)
     """
-               
-    cur_1.execute(postgres_insert_query)    
+
+    cur_1.execute(postgres_insert_query)
     conn_1.commit()
     conn_1.close()
 
@@ -80,10 +78,10 @@ dag = DAG(
     start_date=pendulum.datetime(2023, 9, 1),
     catchup=False,
     tags=["PET-Project", "dds"],
-    is_paused_upon_creation=False
+    is_paused_upon_creation=False,
 )
 
-    # create DAG logic (sequence/order)
+# create DAG logic (sequence/order)
 t1 = DummyOperator(task_id="start")
 t21 = PythonOperator(task_id="managers", python_callable=load_managers_to_dds, dag=dag)
 #   t22 = PythonOperator(task_id="couriers", python_callable=load_paste_data_couriers, dag=dag)
@@ -91,5 +89,5 @@ t21 = PythonOperator(task_id="managers", python_callable=load_managers_to_dds, d
 #   t24 = PythonOperator(task_id="orders", python_callable=load_paste_data_orders, dag=dag)
 #   t25 = PythonOperator(task_id="deliveries", python_callable=load_paste_data_deliveries, dag=dag)
 t4 = DummyOperator(task_id="end")
-    
+
 t1 >> t21 >> t4
