8c3cfb119845070eb9e264b400150b4f46dd181c
aspesivtsev <anspesiv@yahoo.com>
2023-10-11 11:09:16 +0300

    added python operator import

--- dags/download_data_files_from_ftp_dag.py
+++ dags/download_data_files_from_ftp_dag.py
@@ -3,6 +3,7 @@ from library.ftp_download import get_files_from_ftp
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from airflow.hooks.base import BaseHook
 
 conn = BaseHook.get_connection('ftp_conn')
