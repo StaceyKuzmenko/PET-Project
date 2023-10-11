fad1333bf12005f354bd2ec9b7c4f642e88ade79
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-10-11 03:32:40 +0300

    Update manager_dag1.py

--- dags/dds/manager_dag1.py
+++ dags/dds/manager_dag1.py
@@ -22,11 +22,72 @@ from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.operators.dummy import DummyOperator
 from typing import Generator
-import psycopg2
+#import psycopg2
+import psycopg
 from airflow.hooks.base import BaseHook
 from configparser import ConfigParser
-from library.pg_connect import PgConnect
+from contextlib import contextmanager
 
+class PgConnect:
+    def __init__(self, host: str, port: str, db_name: str, user: str, pw: str, sslmode: str = "require") -> None:
+        self.host = host
+        self.port = int(port)
+        self.db_name = db_name
+        self.user = user
+        self.pw = pw
+        self.sslmode = sslmode
+
+    def url(self) -> str:
+        return """
+            host={host}
+            port={port}
+            dbname={db_name}
+            user={user}
+            password={pw}
+            target_session_attrs=read-write
+            sslmode={sslmode}
+        """.format(
+            host=self.host,
+            port=self.port,
+            db_name=self.db_name,
+            user=self.user,
+            pw=self.pw,
+            sslmode=self.sslmode)
+
+    def client(self):
+        return psycopg.connect(self.url())
+
+    @contextmanager
+    def connection(self) -> Generator[psycopg.Connection, None, None]:
+        conn = psycopg.connect(self.url())
+        try:
+            yield conn
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            raise e
+        finally:
+            conn.close()
+
+
+class ConnectionBuilder:
+
+    @staticmethod
+    def pg_conn(conn_id: str) -> PgConnect:
+        conn = BaseHook.get_connection(conn_id)
+
+        sslmode = "require"
+        if "sslmode" in conn.extra_dejson:
+            sslmode = conn.extra_dejson["sslmode"]
+
+        pg = PgConnect(str(conn.host),
+                       str(conn.port),
+                       str(conn.schema),
+                       str(conn.login),
+                       str(conn.password),
+                       sslmode)
+
+        return pg
 
 ### POSTGRESQL settings ###
 # set postgresql connection from basehook
