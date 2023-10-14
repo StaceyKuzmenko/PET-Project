c30243acc2aaf82cea03a4617cb7438256db2bff
aspesivtsev <anspesiv@yahoo.com>
2023-10-14 21:07:25 +0300

    fixing tasks

--- dags/library/managing_files.py
+++ dags/library/managing_files.py
@@ -3,7 +3,7 @@ import os
 def find_the_latest_local_file_by_name(folder:str) -> str:
     file_list = os.listdir(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder))
     latest_file = sorted(file_list, reverse=True)
-    return str(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder, '/')) + str(latest_file[0])
+    return str(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder)) + "/" + str(latest_file[0])
 
 
 if __name__=="__main__":
