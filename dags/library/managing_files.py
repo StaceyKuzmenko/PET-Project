7566d3160cd48e8b7ab56a0e41fe0b62e047c4e9
aspesivtsev <anspesiv@yahoo.com>
2023-10-14 11:55:07 +0300

    fixing file paths in managing_files module

--- dags/library/managing_files.py
+++ dags/library/managing_files.py
@@ -3,8 +3,8 @@ import os
 def find_the_latest_local_file_by_name(folder:str) -> str:
     file_list = os.listdir(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder))
     latest_file = sorted(file_list, reverse=True)
-    return os.listdir(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder, latest_file[0]))
+    return os.listdir(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder)+str(latest_file[0]))
 
 
 if __name__=="__main__":
-    find_the_latest_local_file_by_name('sales')
+    find_the_latest_local_file_by_name()
