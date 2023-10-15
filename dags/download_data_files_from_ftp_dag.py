646946d2da4b73ed0c8b17209f3e5ece63a3b081
aspesivtsev <anspesiv@yahoo.com>
2023-10-15 23:46:45 +0300

    adding execution times to dag

--- dags/download_data_files_from_ftp_dag.py
+++ dags/download_data_files_from_ftp_dag.py
@@ -20,10 +20,10 @@ execution_times = ["5 4", "5 9", "5 13"] # <<< this is in UTC (in UTC +3 07:05; 
 
 with DAG(
     dag_id="download_files_from_ftp_to_local_folders",
-    start_date=datetime.datetime(2023, 10, 5),
+    start_date=datetime.datetime(2023, 10, 14),
     description='Download 3 most recent files from data folders',
     #schedule="@daily",
-    #schedule_interval = None,
+    schedule_interval = '5 4,9,13 * * *',
     catchup=False,
     tags=['ftp'],
     max_active_runs=1
@@ -38,9 +38,9 @@ with DAG(
                    'passwd': conn.password
                    })
     
-for time in execution_times:
+'''for time in execution_times:
     dag.schedule_interval = f'{time} * * *'
-    
+    '''
 (
     download_files
 )
