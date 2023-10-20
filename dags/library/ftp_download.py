512df7f93f7ece87224a0790ae7e182032280fdf
aspesivtsev <anspesiv@yahoo.com>
2023-10-20 23:07:54 +0300

    adding logging and error handling

--- dags/library/ftp_download.py
+++ dags/library/ftp_download.py
@@ -1,48 +1,55 @@
 from ftplib import FTP
 from datetime import datetime
 import os
+import logging
+
 #import pydantic
-#import logger
+logger = logging.getLogger("airflow")
 
 def get_files_from_ftp(folder_list:list, host, user:str, passwd:str):
     #establishing ftp connetion and 
-    ftp = FTP(host)
-    ftp.login(user=user, passwd=passwd)
-    
-    for folder in folder_list:
-        print('FTP path: ', os.path.join('Engeocom', 'test', folder))
-        #changing home folder to Engeocom
-        ftp.cwd(os.path.join('/', 'Engeocom', 'test', folder))
+    try:
+        ftp = FTP(host)
+        ftp.login(user=user, passwd=passwd)
         
-        #getting the list of filenames 
-        filelist = ftp.nlst()
-        
-        #looping through the list of files in a folder
-        for file in filelist:
-            latest_time = None
-            latest_name = None
-            #get the time when the file was modified    
-            modified_time = ftp.sendcmd('MDTM ' + file)
+        for folder in folder_list:
+            logger.info('FTP path: ', os.path.join('Engeocom', 'test', folder))
+            #changing home folder to Engeocom
+            ftp.cwd(os.path.join('/', 'Engeocom', 'test', folder))
             
-            #finding the lates time and filename
-            #we will record the data to 2 separate variables
-            if (latest_time is None) or (modified_time > latest_time):
-                latest_name = file
-                latest_time = modified_time
-            
-            #for debugging printing all files dates
-            print ('found date >>>', datetime.strptime(modified_time[4:], "%Y%m%d%H%M%S").strftime("%d %B %Y %H:%M:%S"))
+            #getting the list of filenames 
+            filelist = ftp.nlst()
+            logger.info(filelist)
 
-        #creating a new name for the file in format yyyymmdd
-        new_file_name = f'{folder}-{latest_time[4:]}.csv'
+            #looping through the list of files in a folder
+            for file in filelist:
+                latest_time = None
+                latest_name = None
+                #get the time when the file was modified    
+                modified_time = ftp.sendcmd('MDTM ' + file)
+                
+                #finding the lates time and filename
+                #we will record the data to 2 separate variables
+                if (latest_time is None) or (modified_time > latest_time):
+                    latest_name = file
+                    latest_time = modified_time
+                
+                #for debugging printing all files dates
+                print ('found date >>>', datetime.strptime(modified_time[4:], "%Y%m%d%H%M%S").strftime("%d %B %Y %H:%M:%S"))
+
+            #creating a new name for the file in format yyyymmdd
+            new_file_name = f'{folder}-{latest_time[4:]}.csv'
+            
+            #copying the file to the local folder
+            file_name_with_full_path_to_local_folder = os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder, new_file_name)
+            with open(file_name_with_full_path_to_local_folder, 'wb') as f:
+                ftp.retrbinary('RETR '+ latest_name, f.write)
         
-        #copying the file to the local folder
-        file_name_with_full_path_to_local_folder = os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder, new_file_name)
-        with open(file_name_with_full_path_to_local_folder, 'wb') as f:
-            ftp.retrbinary('RETR '+ latest_name, f.write)
-    
-    #closing the ftp connetion
-    ftp.close
+        #closing the ftp connetion
+        ftp.close
+    except Exception as err:
+        logger.error(err)
+
 
 
 
