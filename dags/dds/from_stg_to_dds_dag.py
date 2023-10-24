86967be947b5cffbd95b0b7eb04ddd1ef77bb00b
Жуль Кирилл <118897479+KirillZhul@users.noreply.github.com>
2023-10-24 18:03:00 +0300

    Update from_stg_to_dds_dag.py

--- dags/dds/from_stg_to_dds_dag.py
+++ dags/dds/from_stg_to_dds_dag.py
@@ -76,140 +76,139 @@ def load_managers_to_dds():
     conn_1.close()	 
 
 def load_clients_to_dds():
-    try:
-        # fetching time UTC and table
-        fetching_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        current_table = "clients"
+    # fetching time UTC and table
+    fetching_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_table = "clients"
 
-        # load to local to DB (clients)
-        cur_1 = conn_1.cursor()
-        postgres_insert_query = """ 
-        INSERT INTO "DDS".clients(client_id, client, manager_id, sales_channel, region)
-        SELECT DISTINCT os.client_id,
-            os.client,
-            m.id AS manager_id,
-            os.sales_channel,
-            os.region 
-        FROM "STG".old_sales AS os 
-        LEFT JOIN "DDS".clients AS c using(client_id) 
-        LEFT JOIN "DDS".managers AS m using(manager)
-        WHERE NOT EXISTS (
-	    SELECT 1
-	    FROM "DDS".clients
-            WHERE "DDS".clients.client_id = os.client_id 
-        )
-        ;
-        INSERT INTO "DDS".clients(client_id, client, manager_id, sales_channel, region)
-        SELECT DISTINCT s.client_id,
-            s.client,
-            m.id AS manager_id,
-            s.sales_channel,
-            s.region 
-        FROM "STG".sales AS s 
-        LEFT JOIN "DDS".clients AS c using(client_id) 
-        LEFT JOIN "DDS".managers AS m using(manager)
-	WHERE NOT EXISTS (
-	    SELECT 1
-	    FROM "DDS".clients
-            WHERE "DDS".clients.client_id = s.client_id 
-        )
-        ;
-        """
-        cur_1.execute(postgres_insert_query)   
-    except Exception as error:
-        print("Exception TYPE:", type(error))
-    finally:
-        # Close the connection    
-        conn_1.commit()
-        conn_1.close()
+    # load to local to DB (clients)
+    cur_1 = conn_1.cursor()
+    postgres_insert_query = """ 
+    INSERT INTO "DDS".clients(client_id, client, manager_id, sales_channel, region)
+    SELECT DISTINCT os.client_id,
+        os.client,
+        m.id AS manager_id,
+        os.sales_channel,
+        os.region 
+    FROM "STG".old_sales AS os 
+    LEFT JOIN "DDS".clients AS c using(client_id) 
+    LEFT JOIN "DDS".managers AS m using(manager)
+    WHERE NOT EXISTS (
+	SELECT 1
+	FROM "DDS".clients
+        WHERE "DDS".clients.client_id = os.client_id 
+    )
+    ;
+    INSERT INTO "DDS".clients(client_id, client, manager_id, sales_channel, region)
+    SELECT DISTINCT s.client_id,
+        s.client,
+        m.id AS manager_id,
+        s.sales_channel,
+        s.region 
+    FROM "STG".sales AS s 
+    LEFT JOIN "DDS".clients AS c using(client_id) 
+    LEFT JOIN "DDS".managers AS m using(manager)
+    WHERE NOT EXISTS (
+	SELECT 1
+	FROM "DDS".clients
+        WHERE "DDS".clients.client_id = s.client_id 
+    );
+    """
+    cur_1.execute(postgres_insert_query)   
+    conn_1.commit()
+    conn_1.close()
 
 def load_orders_realizations_to_dds():
-    try:
-        # fetching time UTC and table
-        fetching_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        current_table = "orders_realizations"
+    # fetching time UTC and table
+    fetching_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_table = "orders_realizations"
 
-        # load to local to DB (orders_realization)
-        cur_1 = conn_1.cursor()
-        postgres_insert_query = """ 
-        INSERT INTO "DDS".orders_realizations(
-	    client_id, 
-	    order_date, 
-	    order_number, 
-	    realization_date, 
-	    realization_number, 
-	    item_number, 
-	    count, 
-	    price, 
-	    total_sum, 
-	    comment)
-    	    SELECT  
-    	        c.id as client_id, 
-    		to_date(os.order_date, 'DD-MM-YYYY'), 
-    		os.order_number, 
-    		to_date(os.realization_date, 'DD-MM-YYYY'), 
-    		os.realization_number, 
-    		os.item_number, 
-    		os.count, 
-    		CASE          
-          		WHEN os.price ~ E'^[0-9]+\\.[0-9]+$' THEN CAST(os.price AS NUMERIC)
-            	WHEN os.price ~ E'^[0-9]+,[0-9]+$' THEN CAST(REPLACE(os.price, ',', '.') AS NUMERIC)
-            	WHEN os.price ~ E'^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(REPLACE(REPLACE(os.price, ' ', ''), ',', '.') AS NUMERIC)
-            	ELSE NULL -- Handles values with consistent format (either dot or comma)
-        	end as price,
-        	CASE          
-          		WHEN os.total_sum ~ E'^[0-9]+\\.[0-9]+$' THEN CAST(os.total_sum AS NUMERIC)
-            	WHEN os.total_sum ~ E'^[0-9]+,[0-9]+$' THEN CAST(REPLACE(os.total_sum, ',', '.') AS NUMERIC)
-            	WHEN os.total_sum ~ E'^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(REPLACE(REPLACE(os.total_sum, ' ', ''), ',', '.') AS NUMERIC)
-            	ELSE NULL -- Handles values with consistent format (either dot or comma)
-        	end as total_sum,
-    		os.comment  
-	    FROM "STG".old_sales as os
-	    left join "DDS".clients as c using(client_id)	    
-        ;
-        INSERT INTO "DDS".orders_realizations(
-	    client_id, 
-	    order_date, 
-	    order_number, 
-	    realization_date, 
-	    realization_number, 
-	    item_number, 
-	    count, 
-	    price, 
-	    total_sum, 
-	    comment)
-    	    SELECT 
-    		c.id as client_id, 
-    		to_date(s.order_date, 'DD-MM-YYYY'), 
-    		s.order_number, 
-    		to_date(s.realization_date, 'DD-MM-YYYY'), 
-    		s.realization_number, 
-    		s.item_number, 
-    		s.count, 
-    		CASE          
-          		WHEN s.price ~ E'^[0-9]+\\.[0-9]+$' THEN CAST(s.price AS NUMERIC)
-            	WHEN s.price ~ E'^[0-9]+,[0-9]+$' THEN CAST(REPLACE(s.price, ',', '.') AS NUMERIC)
-            	WHEN s.price ~ E'^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(REPLACE(REPLACE(s.price, ' ', ''), ',', '.') AS NUMERIC)
-            	ELSE NULL -- Handles values with consistent format (either dot or comma)
-        	end as price,
-        	CASE          
-          		WHEN s.total_sum ~ E'^[0-9]+\\.[0-9]+$' THEN CAST(s.total_sum AS NUMERIC)
-            	WHEN s.total_sum ~ E'^[0-9]+,[0-9]+$' THEN CAST(REPLACE(s.total_sum, ',', '.') AS NUMERIC)
-            	WHEN s.total_sum ~ E'^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(REPLACE(REPLACE(s.total_sum, ' ', ''), ',', '.') AS NUMERIC)
-            	ELSE NULL -- Handles values with consistent format (either dot or comma)
-        	end as total_sum,
-    		s.comment  
-	    FROM "STG".sales as s
-	    left join "DDS".clients as c using(client_id)	    
-        ;
-        """
-        cur_1.execute(postgres_insert_query)    
-    except Exception as error:
-        print("Exception TYPE:", type(error))
-    finally:
-        # Close the connection    
-        conn_1.commit()
-        conn_1.close()
+    # load to local to DB (orders_realization)
+    cur_1 = conn_1.cursor()
+    postgres_insert_query = """ 
+    INSERT INTO "DDS".orders_realizations(
+      client_id, order_date, order_number, 
+      realization_date, realization_number, 
+      item_number, count, price, total_sum, 
+      comment
+    ) 
+    SELECT 
+      c.id as client_id, 
+      to_date(os.order_date, 'DD-MM-YYYY'), 
+      os.order_number, 
+      to_date(
+        os.realization_date, 'DD-MM-YYYY'
+      ), 
+      os.realization_number, 
+      os.item_number, 
+      os.count, 
+      CASE WHEN os.price ~ E '^[0-9]+\\.[0-9]+$' THEN CAST(os.price AS NUMERIC) WHEN os.price ~ E '^[0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(os.price, ',', '.') AS NUMERIC
+      ) WHEN os.price ~ E '^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(
+          REPLACE(os.price, ' ', ''), 
+          ',', 
+          '.'
+        ) AS NUMERIC
+      ) ELSE NULL -- Handles values with consistent format (either dot or comma)
+      end as price, 
+      CASE WHEN os.total_sum ~ E '^[0-9]+\\.[0-9]+$' THEN CAST(os.total_sum AS NUMERIC) WHEN os.total_sum ~ E '^[0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(os.total_sum, ',', '.') AS NUMERIC
+      ) WHEN os.total_sum ~ E '^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(
+          REPLACE(os.total_sum, ' ', ''), 
+          ',', 
+          '.'
+        ) AS NUMERIC
+      ) ELSE NULL -- Handles values with consistent format (either dot or comma)
+      end as total_sum, 
+      os.comment 
+    FROM 
+      "STG".old_sales as os 
+      left join "DDS".clients as c using(client_id);
+    INSERT INTO "DDS".orders_realizations(
+      client_id, order_date, order_number, 
+      realization_date, realization_number, 
+      item_number, count, price, total_sum, 
+      comment
+    ) 
+    SELECT 
+      c.id as client_id, 
+      to_date(s.order_date, 'DD-MM-YYYY'), 
+      s.order_number, 
+      to_date(
+        s.realization_date, 'DD-MM-YYYY'
+      ), 
+      s.realization_number, 
+      s.item_number, 
+      s.count, 
+      CASE WHEN s.price ~ E '^[0-9]+\\.[0-9]+$' THEN CAST(s.price AS NUMERIC) WHEN s.price ~ E '^[0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(s.price, ',', '.') AS NUMERIC
+      ) WHEN s.price ~ E '^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(
+          REPLACE(s.price, ' ', ''), 
+          ',', 
+          '.'
+        ) AS NUMERIC
+      ) ELSE NULL -- Handles values with consistent format (either dot or comma)
+      end as price, 
+      CASE WHEN s.total_sum ~ E '^[0-9]+\\.[0-9]+$' THEN CAST(s.total_sum AS NUMERIC) WHEN s.total_sum ~ E '^[0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(s.total_sum, ',', '.') AS NUMERIC
+      ) WHEN s.total_sum ~ E '^[0-9]+ [0-9]+,[0-9]+$' THEN CAST(
+        REPLACE(
+          REPLACE(s.total_sum, ' ', ''), 
+          ',', 
+          '.'
+        ) AS NUMERIC
+      ) ELSE NULL -- Handles values with consistent format (either dot or comma)
+      end as total_sum, 
+      s.comment 
+    FROM 
+      "STG".sales as s 
+      left join "DDS".clients as c using(client_id);
+    """
+    cur_1.execute(postgres_insert_query)    
+    conn_1.commit()
+    conn_1.close()
 
 default_args = {
     "owner": "airflow",
