d0aa60867daa90d1407ba206aca74ff99c657b7c
Anastasiya Kuzmenko <113375513+StaceyKuzmenko@users.noreply.github.com>
2023-10-29 21:01:05 +0300

    Update stg_clearing_tables.sql

--- dags/sql/stg_clearing_tables.sql
+++ dags/sql/stg_clearing_tables.sql
@@ -1,3 +1,4 @@
+--чистим таблицы слоя STG перед тем, как залить в них актуальные данные из локальных папок, которые уже находятся на сервере
 TRUNCATE TABLE "STG".sales;
 TRUNCATE TABLE "STG".category;
 TRUNCATE TABLE "STG".forecast;
