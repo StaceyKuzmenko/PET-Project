0070295102d13d7e7dea9ac7ac8660afbffc3b77
StaceyKuzmenko <avasseikina@gmail.com>
2023-11-18 18:01:06 +0300

    new cdm dag from Stacey

--- dags/cmd/from_dds_to_cdm_dag.py
+++ dags/cmd/from_dds_to_cdm_dag.py
@@ -34,11 +34,11 @@ with DAG(
 
     # create DAG logic (sequence/order)
     t1 = DummyOperator(task_id="start")
-    t10 = PostgresOperator(task_id="all_months_to", postgres_conn_id=conn, sql="dags/sql/cdm_load_all_months_aggregated_sales.sql")
-    t11 = PostgresOperator(task_id="brands_to", postgres_conn_id=conn, sql="dags/sql/cdm_load_monthly_sales_by_brands.sql")
-    t12 = PostgresOperator(task_id="channels_to", postgres_conn_id=conn, sql="dags/sql/cdm_load_monthly_sales_by_sales_channels.sql")
-    t13 = PostgresOperator(task_id="aggregates_sales_to", postgres_conn_id=conn, sql="dags/sql/cdm_load_to_current_month_aggregated_sales.sql")
-    t14 = PostgresOperator(task_id="forecast_to", postgres_conn_id=conn, sql="dags/sql/cdm_load_to_monthly_sales_report.sql")
+    t10 = PostgresOperator(task_id="all_months_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_all_months_aggregated_sales.sql")
+    t11 = PostgresOperator(task_id="brands_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_monthly_sales_by_brands.sql")
+    t12 = PostgresOperator(task_id="channels_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_monthly_sales_by_sales_channels.sql")
+    t13 = PostgresOperator(task_id="aggregates_sales_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_to_current_month_aggregated_sales.sql")
+    t14 = PostgresOperator(task_id="forecast_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_to_monthly_sales_report.sql")
     t2 = DummyOperator(task_id="end")
 
     '''for time in execution_times:
