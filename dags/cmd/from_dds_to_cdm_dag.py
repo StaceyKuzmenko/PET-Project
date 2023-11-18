9078f0f4340da504b10ba75a4120ead60f0e7e6a
StaceyKuzmenko <avasseikina@gmail.com>
2023-11-18 19:35:59 +0300

    new dag from Stacey

--- dags/cmd/from_dds_to_cdm_dag.py
+++ dags/cmd/from_dds_to_cdm_dag.py
@@ -6,6 +6,7 @@ from wsgiref import headers
 from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.postgres.operators.postgres import SqlFileOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from airflow.hooks.base import BaseHook
@@ -33,12 +34,18 @@ with DAG(
 ) as dag:
 
     # create DAG logic (sequence/order)
+    
+    def get_sql_file_path(script_name):
+        sql_script_directory = "dags/sql"
+        return f"{sql_script_directory}/{script_name}.sql"
+    
+    t10 = SqlFileOperator(task_id="all_months_to", postgres_conn_id="postgres_local", sql=get_sql_file_path("cdm_load_all_months_aggregated_sales"), autocommit=True, dag=dag,)
+    t11 = SqlFileOperator(task_id="brands_to", postgres_conn_id="postgres_local", sql=get_sql_file_path("cdm_load_monthly_sales_by_brands"), autocommit=True, dag=dag,)
+    t12 = SqlFileOperator(task_id="channels_to", postgres_conn_id="postgres_local", sql=get_sql_file_path("cdm_load_monthly_sales_by_sales_channels"), autocommit=True, dag=dag,)
+    t13 = SqlFileOperator(task_id="aggregates_sales_to", postgres_conn_id="postgres_local", sql=get_sql_file_path("cdm_load_to_current_month_aggregated_sales"), autocommit=True, dag=dag,)
+    t14 = SqlFileOperator(task_id="forecast_to", postgres_conn_id="postgres_local", sql=get_sql_file_path("cdm_load_to_monthly_sales_report"), autocommit=True, dag=dag,)
+
     t1 = DummyOperator(task_id="start")
-    t10 = PostgresOperator(task_id="all_months_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_all_months_aggregated_sales.sql")
-    t11 = PostgresOperator(task_id="brands_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_monthly_sales_by_brands.sql")
-    t12 = PostgresOperator(task_id="channels_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_monthly_sales_by_sales_channels.sql")
-    t13 = PostgresOperator(task_id="aggregates_sales_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_to_current_month_aggregated_sales.sql")
-    t14 = PostgresOperator(task_id="forecast_to", postgres_conn_id="postgres_local", sql="dags/sql/cdm_load_to_monthly_sales_report.sql")
     t2 = DummyOperator(task_id="end")
 
     '''for time in execution_times:
