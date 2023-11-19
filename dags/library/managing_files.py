7cb9295c10d82fdd56a61943162178222189e0f3
aspesivtsev <anspesiv@yahoo.com>
2023-11-19 15:39:48 +0300

    fixing files_dir

--- dags/library/managing_files.py
+++ dags/library/managing_files.py
@@ -1,5 +1,7 @@
 import os
 
+local_dir = '/opt/files_dir/'
+
 def find_the_latest_local_file_by_name(folder:str) -> str:
     """Сканируем папку и находим самый новый файл в ней (по имени)"""
     file_list = os.listdir(os.path.join('/', 'opt', 'airflow', 'plugins', 'files_dir', folder))
@@ -7,7 +9,7 @@ def find_the_latest_local_file_by_name(folder:str) -> str:
     if len(latest_file) == 0:
         return ''
     else:
-        return '/opt/files_dir/' + str(folder) + '/' + str(latest_file[0])
+        return local_dir + str(folder) + '/' + str(latest_file[0])
 
 
 if __name__=="__main__":
